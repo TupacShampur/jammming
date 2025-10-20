041be9b5663193f3f23840607e9ab6a0a0217ab0
TupacShampur <roma.ivashckevych@gmail.com>
2025-10-20 12:31:16 +0200

    save logic add

--- src/App.js
+++ src/App.js
@@ -6,9 +6,27 @@ import Playlist from "./components/Playlist/Playlist";
 
 function App() {
   const mockTracks = [
-    { id: 1, name: "Song 1", artist: "Artist 1", album: "Album 1" },
-    { id: 2, name: "Song 2", artist: "Artist 2", album: "Album 2" },
-    { id: 3, name: "Song 3", artist: "Artist 3", album: "Album 3" },
+    {
+      id: 1,
+      name: "Song 1",
+      artist: "Artist 1",
+      album: "Album 1",
+      uri: "spotify:track:2fi69bgAKtSBFnCGCTNQFp",
+    },
+    {
+      id: 2,
+      name: "Song 2",
+      artist: "Artist 2",
+      album: "Album 2",
+      uri: "spotify:track:7i3JVEk8OkTk8z5ZreAq2F",
+    },
+    {
+      id: 3,
+      name: "Song 3",
+      artist: "Artist 3",
+      album: "Album 3",
+      uri: "spotify:track:6GyDY0yE47rfk8pcuKhioh",
+    },
   ];
 
   const [tracklist, setTracklist] = useState([]);
@@ -28,6 +46,13 @@ function App() {
     });
   };
 
+  const savePlaylist = () => {
+    const trackUris = tracklist.map((track) => track.uri);
+    console.log(trackUris);
+
+    setTracklist([]);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -42,6 +67,7 @@ function App() {
             tracks={tracklist}
             onRemove={removeTrack}
             isRemoval={true}
+            onSave={savePlaylist}
           />
         </div>
       </header>
--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import styles from "./Playlist.module.css";
 import Tracklist from "../Tracklist/Tracklist";
 
-function Playlist({ tracks, onRemove, isRemoval }) {
+function Playlist({ tracks, onRemove, isRemoval, onSave }) {
   return (
     <>
       <div className={styles.playlist}>
@@ -11,6 +11,12 @@ function Playlist({ tracks, onRemove, isRemoval }) {
           placeholder="Edit playlist name..."
         ></input>
         <Tracklist tracks={tracks} onRemove={onRemove} isRemoval={isRemoval} />
+        <button
+          className={styles.saveButton}
+          onClick={() => onSave && onSave()}
+        >
+          Save
+        </button>
       </div>
     </>
   );
